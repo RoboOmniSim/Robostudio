74255629dcf52b1c7515ec44dcc4be5f2db727ea
louhz <haozhelo@usc.edu>
2024-06-20 00:48:11 -0700

    update parameter

--- nerfstudio/robotic/config/omnisim_base.py
+++ nerfstudio/robotic/config/omnisim_base.py
@@ -20,3 +20,34 @@ class MetaConfig :
 
         for key in another.keys() :
             setattr(self, key, another[key])
+
+
+
+class MetaGS(MetaConfig):
+
+    # this is for pass parameter from gs to omnisim
+
+    def __init__(self, config : dict):
+
+        # 
+
+        self.width = config["width"]
+        self.height = config["height"]
+        self.horizontal_fov = config["horizontal_fov"]
+        self.enable_tensors = config["enable_tensors"]
+        self.camera_locate_pos = config["camera_locate_pos"]
+        self.camera_target_pos = config.get("camera_target_pos", None)
+        self.camera_rot = config.get("camera_rot", None)
+
+        self.check()
+    
+    def check(self) :
+
+        if self.camera_target_pos != None and self.camera_rot != None:
+
+            print("Warning: camera target and rot are both set, target will be ignored")
+        
+        if self.camera_target_pos == None and self.camera_rot == None:
+
+            print("Warning: camera target and rot are both not set, default target will be used")
+            self.camera_target_pos = [0, 0, 0]
